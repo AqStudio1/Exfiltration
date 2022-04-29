91b85b2934c666c673c726ffee81ab48933e1653
AqStudio1 <zaghum.mughal.1@gmail.com>
2022-04-29 12:19:18 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 $SMTPServer = 'smtp.gmail.com'
-$SMTPInfo = New - Object Net.Mail.SmtpClient($SMTPServer, 587)
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SMTPServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New - Object System.Net.NetworkCredential('zaghum.mughal.1@gmail.com', ',.H@cked12,.')
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('zaghum.mughal.1@gmail.com', ',.H@cked12,.')
 $message = New - Object System.Net.Mail.MailMessage
 $message.From = 'zaghum.mughal.1@gmail.com'
 $message.To.Add('zaghum.mughal.1@gmail.com')
